7c794c40bbbef2dbd07475e348f5c0f6afc4c3f7
Ana-Leyva <119750496+Ana-Leyva@users.noreply.github.com>
2022-12-03 11:04:09 -0600

    Inicio de repositorio
    Carga de index, estilos, js e imágenes

---
+++ main.js
@@ -0,0 +1,25 @@
+let app = document.getElementById('typewriter');
+
+ 
+
+let typewriter = new Typewriter(app, {
+
+  loop: true,
+
+  delay: 75,
+
+});
+
+ 
+
+typewriter
+
+  .pauseFor(2500)
+
+  .typeString('conoceme un poco.')
+
+  .pauseFor(200)
+
+  .deleteChars(10)
+
+  .start();
